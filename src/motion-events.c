ea497ab086243cf21369f030f4438a9905f052ab
Aaron Ayub <55106255+AaronAyub@users.noreply.github.com>
2024-04-11 18:03:58 -0400

    Implement update_edges function

--- src/motion-events.c
+++ src/motion-events.c
@@ -8,6 +8,14 @@
 #include <math.h>
 
 #include "motion-events.h"
+#include "crop-area.h"
+
+static inline int min (int x, int y) {
+  return x < y ? x : y;
+}
+static inline int max (int x, int y) {
+  return x > y ? x : y;
+}
 
 void select_edges (GtkGestureDrag *self, gdouble x, gdouble y, gpointer user_data) {
   MotionParams *mp = user_data;
@@ -31,6 +39,39 @@ void select_edges (GtkGestureDrag *self, gdouble x, gdouble y, gpointer user_dat
     mp->move_edges |= FC_MV_TOP;
   }
 }
-void update_edges (GtkGestureDrag *self, gdouble x, gdouble y, gpointer user_data) {
 
+void update_edges (GtkGestureDrag *self, gdouble x, gdouble y, gpointer user_data) {
+  MotionParams *mp = user_data;
+  GtkWidget *da = GTK_WIDGET (mp->draw_area);
+  CropArea *ca = mp->crop_area;
+
+  double start_x = 0;
+  double start_y = 0;
+  gtk_gesture_drag_get_start_point (self, &start_x, &start_y);
+
+  int max_width = gdk_pixbuf_get_width (mp->pixbuf);
+  int max_height = gdk_pixbuf_get_height (mp->pixbuf);
+
+  int new_x = (int) ((start_x + x) / gtk_widget_get_width (da) * max_width);
+  int new_y = (int) ((start_y + y) / gtk_widget_get_height (da) * max_height);
+
+  // Recalculate edge positions
+  if (mp->move_edges & FC_MV_LEFT) {
+    mp->left = min (max (new_x, 0), max_width);
+  } else if (mp->move_edges & FC_MV_RIGHT) {
+    mp->right = min (max (new_x, 0), max_width);
+  }
+  if (mp->move_edges & FC_MV_TOP) {
+    mp->top = min (max (new_y, 0), max_height);
+  } else if (mp->move_edges & FC_MV_BOTTOM) {
+    mp->bottom = min (max (new_y, 0), max_height);
+  }
+
+  // Recalculate CropArea
+  ca->x = min (mp->left, mp->right);
+  ca->y = min (mp->top, mp->bottom);
+  ca->width = abs (mp->left - mp->right);
+  ca->height = abs (mp->top - mp->bottom);
+
+  gtk_widget_queue_draw (da);
 }
